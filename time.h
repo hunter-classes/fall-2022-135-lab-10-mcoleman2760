20d6222559242ba711919d8b83d93d2e4034b0e8
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-11-18 01:47:09 -0500

    Update time.h

--- time.h
+++ time.h
@@ -5,23 +5,5 @@ public:
     int h;
     int m;
 };
-enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
-class Movie { 
-public: 
-    std::string title;
-    Genre genre;     
-    int duration;    
-};
-class TimeSlot { 
-public: 
-    Movie movie;     
-    Time startTime;   
-};
 
-int minutesSinceMidnight(Time time);
-int minutesUntil(Time earlier, Time later);
-Time addMinutes(Time time0, int min);
-std::string getTimeSlot(TimeSlot ts);
-void printTime(Time time);
-TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie);
-bool timeOverlap(TimeSlot ts1, TimeSlot ts2); 
+
