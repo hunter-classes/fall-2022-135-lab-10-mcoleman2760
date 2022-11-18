f31ad2f4157c3de48892daaea43b105aaef10d6e
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-11-18 01:44:44 -0500

    Create funcs.h

---
+++ funcs.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include "time.h"
+#include "movie.h"
+#include "timeslot.h"
+
+int minutesSinceMidnight(Time time);
+int minutesUntil(Time earlier, Time later);
+Time addMinutes(Time time0, int min);
+std::string getTimeSlot(TimeSlot ts);
+void printTime(Time time);
+TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie);
+bool timeOverlap(TimeSlot ts1, TimeSlot ts2); 
